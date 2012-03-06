ef8b74954816a6e83f125e09ebd670f6b7e3b8d9
przemyslaw.strumnik@gmail.com <przemyslaw.strumnik@gmail.com@7dedb6f7-01c4-33de-85ba-19ef4ab5aebf>
2012-03-06 14:01:59 +0000

    refactoring

---
+++ trunk/iReport/src/com/ireport/Report.java
@@ -0,0 +1,35 @@
+package com.ireport;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Map;
+
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+
+public class Report {
+	
+	public static JasperPrint fillReport(Map<String, Object> param)
+	{
+		JasperPrint print = null;
+		try {
+//			JasperDesign jasperDesign = JRXmlLoader.load("wizardstep.jrxml");
+//			JasperReport jasperReport = JasperCompileManager.compileReport(jasperDesign);
+			
+			Connection conn = null;		  
+			// Load the database driver
+		    Class.forName("org.postgresql.Driver") ;
+		    // Get a connection to the database		      
+		    conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/quamasoft", 
+		    	  "root",  
+		    	  "kom#34ala");
+			
+			print = JasperFillManager.fillReport("wizardstep.jasper", param, conn);
+//			JasperExportManager.exportReportToPdfFile(print, "pdf.pdf");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return print;
+	}
+
+}
