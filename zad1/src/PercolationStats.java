086faab6b3b2540431b4dacbb147d5e24f2a4aac
przemyslaw.strumnik <przemyslaw.strumnik@gmail.com>
2013-02-14 14:19:08 +0000

    LOG: zad1

--- zad1/src/PercolationStats.java
+++ zad1/src/PercolationStats.java
@@ -1,21 +1,31 @@
 public class PercolationStats {
     
-    private Percolation percolation;
+    private int[] openedSitesCount;
     
     // perform T independent computational experiments on an N-by-N grid
     public PercolationStats(int N, int T) {
         if ((N <= 0)  || (T <= 0))
             throw new IllegalArgumentException();
         
-        percolation = new Percolation(N);
+        openedSitesCount = new int[T];
         for (int i = 0; i < T; i++){
-            
+            Percolation pre = new Percolation(N);
+            int count = 0;
+            while (!pre.percolates()) {
+                int p = StdRandom.uniform(1, N+1);
+                int q = StdRandom.uniform(1, N+1);
+                if (!pre.isOpen(p, q)){
+                    pre.open(p, p);
+                    count++;
+                }
+            }
+            openedSitesCount[i] = count;
         }
     }
 
     // sample mean of percolation threshold
     public double mean() {
-        return 0.0;
+        return StdStats.mean(openedSitesCount);
     }
 
     // sample standard deviation of percolation threshold
@@ -35,5 +45,7 @@ public class PercolationStats {
 
     // test client, described below
     public static void main(String[] args) {
+        PercolationStats pStat = new PercolationStats(10, 2);
+        StdOut.println(pStat.mean());
     }
 }
