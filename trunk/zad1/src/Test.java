2ee4fb717b8fabaa2e9d5c743b9ddea6bf723e1c
przemyslaw.strumnik@gmail.com <przemyslaw.strumnik@gmail.com@7dedb6f7-01c4-33de-85ba-19ef4ab5aebf>
2013-02-14 13:50:53 +0000

    LOG: zad1

---
+++ trunk/zad1/src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+
+    public static void main(String[] args) {
+        StdOut.println("asdasd");
+        // StdRandom
+        // if (i <= 0 || i > N)
+        // throw new IndexOutOfBoundsException("row index i out of bounds");
+    }
+}
