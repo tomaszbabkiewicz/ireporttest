b19aab053c9ebd46b033e62e52cd5cac52a96422
przemyslaw.strumnik@gmail.com <przemyslaw.strumnik@gmail.com@7dedb6f7-01c4-33de-85ba-19ef4ab5aebf>
2013-02-25 18:05:24 +0000

    LOG: zad2

--- trunk/zad2/src/RandomizedQueue.java
+++ trunk/zad2/src/RandomizedQueue.java
@@ -6,6 +6,7 @@ import java.util.NoSuchElementException;
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
     private int size = 0;
+    private int maxsize = 0;
     private ArrayList<Item> randomizedQueue;
     
     // construct an empty randomized queue
@@ -29,28 +30,32 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
             throw new NullPointerException();
         randomizedQueue.add(item);
         size++;
+        if (size > maxsize)
+            maxsize = size;
     }
 
     // delete and return a random item
     public Item dequeue() {
         if (isEmpty())
             throw new NoSuchElementException();
-        int ind = 0;
-        if (size > 0)
-            ind = StdRandom.uniform(size);
+        if (size > 1) {
+            int ind = StdRandom.uniform(size);
+            Collections.swap(randomizedQueue, ind, size - 1);
+        }
         size--;
-        Collections.swap(randomizedQueue, ind, size);
-        return randomizedQueue.remove(ind);
+        Item i = randomizedQueue.remove(size);
+        if (size > 0 && (size == maxsize/2)) {
+            randomizedQueue.trimToSize();
+            maxsize = size;
+        }
+        return i;
     }
 
     // return (but do not delete) a random item
     public Item sample() {
         if (isEmpty())
             throw new NoSuchElementException();
-        int ind = 0;
-        if (size > 0)
-            ind = StdRandom.uniform(size);
-        return randomizedQueue.get(ind);
+        return randomizedQueue.get(StdRandom.uniform(size));
     }
     
     // return an independent iterator over items in random order
